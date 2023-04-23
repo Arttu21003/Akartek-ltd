70f4282409af6f252c07f1fd400f6c4238f98091
Arttu21003 <91421395+Arttu21003@users.noreply.github.com>
2023-04-23 16:53:24 +0300

    Create index.php

---
+++ php-files/index.php
@@ -0,0 +1,30 @@
+<?php
+
+// Company information
+$company_name = "Akartek";
+$company_description = "Akartek is a technology company that provides innovative solutions to transform the business processes and enhance the digital experience for clients.";
+
+// Product information
+$product_name = "Akartek Solutions";
+$product_description = "Our solutions are designed to address the technology challenges faced by businesses, including cloud computing, artificial intelligence, cybersecurity, and enterprise mobility.";
+
+// Contact information
+$company_email = "info@akartek.com";
+$company_phone = "+1 (123) 456-7890";
+$company_address = "123 Main St, Anytown, USA";
+
+// Display company information
+echo "<h1>$company_name</h1>";
+echo "<p>$company_description</p>";
+
+// Display product information
+echo "<h2>$product_name</h2>";
+echo "<p>$product_description</p>";
+
+// Display contact information
+echo "<h3>Contact Us</h3>";
+echo "<p>Email: $company_email</p>";
+echo "<p>Phone: $company_phone</p>";
+echo "<p>Address: $company_address</p>";
+
+?>
